ef2aaea48ba7bf2c2d0f111c623406d83deb2068
Pierre Irby <stoked49@yahoo.com>
2017-01-24 15:27:40 -0600

    mySQL commit.

---
+++ src/select_exercise.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT release_date FROM albums WHERE name = 'Sgt. Peppers Lonely Hearts Club Band';
+
+SELECT genre FROM albums WHERE name = 'Nevermind';
+
+SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+
+SELECT name FROM albums WHERE sales < 20000000;
+
+SELECT name  FROM albums WHERE genre LIKE '%rock%';
