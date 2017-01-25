cc3839b9ef7a06776784a5d61015994f490d0b9c
Pierre Irby <stoked49@yahoo.com>
2017-01-25 11:49:34 -0600

    mySQL commit.

--- src/where_exercises.sql
+++ src/where_exercises.sql
@@ -1,6 +1,6 @@
 SELECT emp_no, first_name, last_name
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE first_name IN ('Irena', 'Vidya') OR first_name IN ('Maya');
 
 SELECT emp_no, first_name, last_name
 FROM employees
