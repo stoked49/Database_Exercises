a24c17320b74c6ae177cd2be41ff34718831c953
Pierre Irby <stoked49@yahoo.com>
2017-01-26 13:48:30 -0600

    mySQL Indexes Lesson commit.

---
+++ src/index_exercise.sql
@@ -0,0 +1,13 @@
+USE employees;
+DESCRIBE departments;
+DESCRIBE dept_emp;
+DESCRIBE dept_manager;
+DESCRIBE employees;
+DESCRIBE salaries;
+DESCRIBE titles;
+
+USE codeup_test_db;
+ALTER TABLE albums ADD INDEX  uniquealbums (artist, name);
+DESCRIBE albums;
+
+show create table albums;
